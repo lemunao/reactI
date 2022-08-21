fadace9146340298e44fecb7596c488cb0e442bd
lemunao <pedroandres1@gmail.com>
2022-08-21 00:40:03 -0400

    project

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+const Footer = (props) => {
+    return (
+        <div className="header-align">
+                    <iframe width="301" height="201" src="https://www.youtube.com/embed/4uziVEZtcoE" title="Video reportaje: Historia de Megaman - Parte 1 (2/2)" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>            
+     
+            <h3>Megaman stages</h3>
+            <h4>{props.title} </h4>
+            
+        </div>
+    );
+};
+export default Footer;
